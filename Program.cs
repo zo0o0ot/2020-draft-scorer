58e8d9f4544b1eb38abfb4ce93fcfef1aaecbc63
Ross Larson <zo0o0ot@gmail.com>
2020-04-23 22:45:10 +0000

    just kidding, get a CSV instead of scraping

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using System;
 using CsvHelper;
-using HtmlAgilityPack;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,15 +13,7 @@ namespace _2020_draft_scorer
         {
             Console.WriteLine("Hello World!");
             var webGet = new HtmlWeb();
-            webGet.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0";
-            var document1 = webGet.Load("https://www.spotrac.com/nfl/draft/2019/");
-
-            Console.WriteLine(document1.ParsedText);
-            //#content > table:nth-child(9)
-            //html body div#outer div#wrapper2 div#content table
-            ///html/body/div[3]/div[3]/div[1]/table[1]
-
-            //List<DraftPick> list1 = getDraft(document1);
+            // Let's get the draft from a CSV instead of scraping it.
         }
 
         public static List<DraftPick> getDraft(HtmlAgilityPack.HtmlDocument doc)
