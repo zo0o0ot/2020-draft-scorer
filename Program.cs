214d8f37a09b01c8d481eaef4ff28112ee09b5fc
Ross Larson <zo0o0ot@gmail.com>
2020-04-23 01:17:57 +0000

    initial program creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020_draft_scorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
